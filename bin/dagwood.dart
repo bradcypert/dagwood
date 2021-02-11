f6f17459d5a7befe1c75b6b489137d3e382133c9
Brad Cypert <brad.cypert@gmail.com>
2021-02-10 20:13:16 -0500

    initial commit

---
+++ bin/dagwood.dart
@@ -0,0 +1,5 @@
+import 'package:dagwood/dagwood.dart' as dagwood;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dagwood.calculate()}!');
+}
---
+++ test/dagwood_test.dart
@@ -0,0 +1,8 @@
+import 'package:dagwood/dagwood.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
